409ece05971746d290d479c5c036c40485657482
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-09-29 18:52:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import os
 import time
 import glob
-import os
 from gtts import gTTS
 from PIL import Image
 import base64
@@ -11,8 +10,7 @@ st.title("Conversión de Texto a Audio")
 image = Image.open('gato_raton.png')
 st.image(image, width=350)
 with st.sidebar:
-    st.subheader("Esrcibe y/o selecciona texto para ser escuchado.")
-
+    st.subheader("Escribe y/o selecciona texto para ser escuchado.")
 
 try:
     os.mkdir("temp")
@@ -28,72 +26,40 @@ st.write('¡Ay! -dijo el ratón-. El mundo se hace cada día más pequeño. Al p
          ' Franz Kafka.'
         
         )
-           
-st.markdown(f"Quieres escucharlo?, copia el texto")
-text = st.text_area("Ingrese El texto a escuchar.")
 
-tld='com'
+st.markdown(f"¿Quieres escucharlo?, copia el texto")
+text = st.text_area("Ingrese el texto a escuchar.")
+
+# Selección de idioma y mostrar la bandera correspondiente
 option_lang = st.selectbox(
     "Selecciona el lenguaje",
-    ("Español", "English", "Deutsch", "Français"))
-if option_lang=="Español" :
-    lg='es'
-if option_lang=="English" :
-    lg='en'
-if option_lang=="Deutsch":
-    lg="de"
-if option_lang=="Français":
-    lg="fr"
+    ("Español", "English", "Deutsch", "Français", "Português")
+)
 
-def text_to_speech(text, tld,lg):
-    
-    tts = gTTS(text,lang=lg) # tts = gTTS(text,'en', tld, slow=False)
+if option_lang == "Español":
+    lg = 'es'
+    flag = Image.open('banderas/espanol.png')
+elif option_lang == "English":
+    lg = 'en'
+    flag = Image.open('banderas/ingles.png')
+elif option_lang == "Deutsch":
+    lg = "de"
+    flag = Image.open('banderas/aleman.png')
+elif option_lang == "Français":
+    lg = "fr"
+    flag = Image.open('banderas/frances.png')
+elif option_lang == "Português":
+    lg = "pt-BR"
+    flag = Image.open('banderas/brasil.png')
+
+# Mostrar la bandera seleccionada
+st.image(flag, width=100)
+
+def text_to_speech(text, lg):
+    tts = gTTS(text, lang=lg)
     try:
         my_file_name = text[0:20]
     except:
         my_file_name = "audio"
     tts.save(f"temp/{my_file_name}.mp3")
     return my_file_name, text
-
-
-#display_output_text = st.checkbox("Verifica el texto")
-
-if st.button("convertir a Audio"):
-     result, output_text = text_to_speech(text, 'com',lg)#'tld
-     audio_file = open(f"temp/{result}.mp3", "rb")
-     audio_bytes = audio_file.read()
-     st.markdown(f"## Tú audio:")
-     st.audio(audio_bytes, format="audio/mp3", start_time=0)
-
-     #if display_output_text:
-     
-     #st.write(f" {output_text}")
-    
-#if st.button("ElevenLAabs",key=2):
-#     from elevenlabs import play
-#     from elevenlabs.client import ElevenLabs
-#     client = ElevenLabs(api_key="a71bb432d643bbf80986c0cf0970d91a", # Defaults to ELEVEN_API_KEY)
-#     audio = client.generate(text=f" {output_text}",voice="Rachel",model="eleven_multilingual_v1")
-#     audio_file = open(f"temp/{audio}.mp3", "rb")
-
-     with open(f"temp/{result}.mp3", "rb") as f:
-         data = f.read()
-
-     def get_binary_file_downloader_html(bin_file, file_label='File'):
-        bin_str = base64.b64encode(data).decode()
-        href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}</a>'
-        return href
-     st.markdown(get_binary_file_downloader_html("audio.mp3", file_label="Audio File"), unsafe_allow_html=True)
-
-def remove_files(n):
-    mp3_files = glob.glob("temp/*mp3")
-    if len(mp3_files) != 0:
-        now = time.time()
-        n_days = n * 86400
-        for f in mp3_files:
-            if os.stat(f).st_mtime < now - n_days:
-                os.remove(f)
-                print("Deleted ", f)
-
-
-remove_files(7)
